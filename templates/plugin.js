79f29a8f914476dcb38f46a6dffe2a24295bca6a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-11 02:34:31 +0100

    :sparkles: (plugin) inject sounds from config and expose instances on injection

--- templates/plugin.js
+++ templates/plugin.js
@@ -1,5 +1,19 @@
+import useSound from 'vue-use-sound'
+import options from './sounds'
 
-/**
- * @type {import('@nuxt/types').Plugin}
- */
-export default ({ app, store }, inject) => {}
+export default ({app}, inject) => {
+  const $sounds = {}
+
+  app.setup = (_, ctx) => {
+    for (const sound of Object.entries(options)) {
+      const [play, options] = useSound(sound[1].src, sound[1].options ? sound[1].options : {})
+
+      $sounds[sound[0]] = {
+        ...options,
+        play
+      }
+    }
+  }
+
+  inject('sounds', $sounds)
+}

156544af8be6f34d3f78150cc00563271a2a208c
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-11 02:14:00 +0100

    :sparkles: (module) improve filenames and config

--- src/module.ts
+++ src/module.ts
@@ -1,40 +1,32 @@
 import { Module } from '@nuxt/types'
 import defu from 'defu'
-// eslint-disable-next-line import/order
 import { resolve } from 'path'
-import { ComposableOptions, ExposedData } from 'vue-use-sound/dist/esm/src/types'
+import {
+  ComposableOptions,
+  ExposedData
+} from 'vue-use-sound/dist/esm/src/types'
 
 export type Sound = {
-  src: string
-  options: ComposableOptions
-}
+  src: string;
+  options: ComposableOptions;
+};
 
-export interface ModuleOptions {
-  references: Sound[]
-}
-
-const DEFAULTS: ModuleOptions = {
-  references: []
-}
+const DEFAULTS: Sound[] = []
 
 const CONFIG_KEY = 'sounds'
 
-const nuxtModule: Module<ModuleOptions> = /* async */ function (moduleOptions) {
-  const options = defu<Sound[]>(
-    this.options[CONFIG_KEY],
-    moduleOptions,
-    DEFAULTS
-  )
+const nuxtModule: Module<Sound[]> = function (sounds) {
+  const options = defu<Sound[]>(this.options[CONFIG_KEY], sounds, DEFAULTS)
 
   this.addTemplate({
-    fileName: 'sounds/options.js',
+    fileName: 'nuxt-use-sound/options.js',
     src: resolve(__dirname, '../templates', 'options.js'),
     options
   })
 
   this.addPlugin({
-    src: resolve(__dirname, '../templates/plugin.js'),
-    fileName: 'nuxt-use-sound.js'
+    src: resolve(__dirname, '../templates', 'plugin.js'),
+    fileName: 'nuxt-use-sound/plugin.js'
   })
 };
 
@@ -48,7 +40,7 @@ declare module '@nuxt/types' {
     [CONFIG_KEY]?: Sound[];
   } // Nuxt 2.9 - 2.13
   interface Context {
-    $sounds: ExposedData[]
+    $sounds: ExposedData[];
   }
 }
 

acfd934a12ac8df4cb6b4ab4b0d6bc53bd20d908
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-24 01:00:30 +0100

    :sparkles: (demo) update demo config

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,16 +1,7 @@
 export default {
-  buildModules: ['@nuxtjs/composition-api'],
   modules: ['../src/module.ts'],
-  sounds: {
-    back: {
-      src: '/back.wav',
-      options: {
-        volume: 0.1
-      }
-    }
-  },
   build: {
-    extend (config) {
+    extend(config) {
       config.module.rules.push({
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
         loader: 'file-loader',
